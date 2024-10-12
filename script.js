a9634660a95f1ecc736bb75b1c47caa23359646b
Itxadii161 <itxadii161@gmail.com>
2024-10-12 06:30:03 -0700

    object of pet created

---
+++ script.js
@@ -0,0 +1,49 @@
+var pet = {
+    name : prompt("Give your pet a name.") ,
+    type : prompt('What kind of pet is it? (e.g., "dog", "cat", "dragon").') ,
+    age : +prompt('How old is your pet?'),
+    happiness : +prompt('How happy is your pet? (a number between 0 and 100).'),
+    hunger : +prompt('How hungry is your pet? (a number between 0 and 100).'),
+
+// feed(): This should decrease the hunger level by 20, but it can’t go below 0.
+    feed : function(){
+        if(this.hunger > 0) {
+            if(this.hunger > 20) {
+                this.hunger = this.hunger - 20;
+                alert(`${this.hunger} is your current hunger level.`)
+            }
+            else if(this.hunger < 0) {
+                alert("No need to feed")
+            }
+        }else {
+            this.hunger = 0 ;
+        }
+    },
+
+    // play(): This should increase happiness by 20, but it can’t exceed 100.
+    
+    play : function(){
+        if(this.happiness > 0) {
+            if(this.happiness < 100) {
+                this.happiness += 20;
+                alert(`${this.happiness} is current happiness level`)
+            }
+            else{
+                alert("happiness level is full 100%.")
+            }
+        } else{
+            this.happiness = 0 ;
+        }
+    },
+
+    // agePet(): This increases the age by 1, decreases happiness by 5, and increases hunger by 10.
+
+    agePet : function() {
+        this.age += 1;
+        this.happiness -= 5;
+        this.hunger -= 10;
+        alert(`${this.name} is ${this.age} ${this.hunger} ${this.happiness}`)
+    }
+}
+
+
